a09de512b266dff4f93e2ba936033cf1eb709fe4
ElenaM <115351366+LenaMishanina@users.noreply.github.com>
2024-10-21 12:21:34 +0300

    onCeate onStart onResume ... Logcat

--- MyFirstApplication/app/src/main/java/com/example/myfirstapplication/MainActivity.kt
+++ MyFirstApplication/app/src/main/java/com/example/myfirstapplication/MainActivity.kt
@@ -2,10 +2,48 @@ package com.example.myfirstapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
+    //код не исполняется
+    //возможно присвоение на уровне класса
+    private var counter = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        counter = 5+6
+        Log.d("tagACTIVITY", "onCreate")
+
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d("tagACTIVITY", "onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d("tagACTIVITY", "onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d("tagACTIVITY", "onPause")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d("tagACTIVITY", "onDestroy")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d("tagACTIVITY", "onStop")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.d("tagACTIVITY", "onRestart")
     }
 }
