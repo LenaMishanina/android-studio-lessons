bd3bace9bc3baedc28ef95a6c8d0f81b06e89b14
ElenaM <115351366+LenaMishanina@users.noreply.github.com>
2024-10-29 01:10:34 +0300

    resources imageView too large image

--- MyFirstApplication/app/src/main/java/com/example/myfirstapplication/MainActivity.kt
+++ MyFirstApplication/app/src/main/java/com/example/myfirstapplication/MainActivity.kt
@@ -13,22 +13,44 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         bind = ActivityMainBinding.inflate(layoutInflater)
         setContentView(bind.root)//root - контейнер, где лежат все элементы
-
+        val a: Int = R.drawable.kate
         bind.btnGetResult.setOnClickListener {
             val workerName = bind.spinnerName.selectedItem.toString()
             Log.d("MainActivityLog", "Result = $workerName")
             if (bind.edPassword.text.toString() == ""){
                 bind.tvResult.text = "Введите пароль"
             } else {
-                bind.tvResult.text = when (workerName) {
-                    Workers.PROGRAMMER.nameWorker -> getSalary(Workers.PROGRAMMER)
-                    Workers.ARCHITECTURE.nameWorker -> getSalary(Workers.ARCHITECTURE)
-                    Workers.COMPOSITOR.nameWorker -> getSalary(Workers.COMPOSITOR)
-                    Workers.DOCTOR.nameWorker -> getSalary(Workers.DOCTOR)
-                    else -> "I don't understand you"
+                when (workerName) {
+                    Workers.PROGRAMMER.nameWorker -> {
+                        bind.tvResult.text = getSalary(Workers.PROGRAMMER)
+                        if (bind.tvResult.text != "Неверно введен пароль")
+                            bind.photoWorker.setImageResource(Workers.PROGRAMMER.photo)
+                    }
+                    Workers.ARCHITECTURE.nameWorker -> {
+                        bind.tvResult.text = getSalary(Workers.ARCHITECTURE)
+                        if (bind.tvResult.text != "Неверно введен пароль")
+                            bind.photoWorker.setImageResource(Workers.ARCHITECTURE.photo)
+                    }
+                    Workers.COMPOSITOR.nameWorker -> {
+                        bind.tvResult.text = getSalary(Workers.COMPOSITOR)
+                        if (bind.tvResult.text != "Неверно введен пароль")
+                            bind.photoWorker.setImageResource(Workers.COMPOSITOR.photo)
+                    }
+                    Workers.DOCTOR.nameWorker -> {
+                        bind.tvResult.text = getSalary(Workers.DOCTOR)
+                        if (bind.tvResult.text != "Неверно введен пароль")
+                            bind.photoWorker.setImageResource(Workers.DOCTOR.photo)
+                    }
+                    else -> {
+                        val errorText = "I don't understand you"
+                        bind.tvResult.text = errorText
+                    }
                 }
             }
             bind.tvResult.visibility = View.VISIBLE
+            if (bind.tvResult.text != "Неверно введен пароль") {
+                bind.photoWorker.visibility = View.VISIBLE
+            }
         }
     }
     private fun getSalary(worker : Workers) : String {
