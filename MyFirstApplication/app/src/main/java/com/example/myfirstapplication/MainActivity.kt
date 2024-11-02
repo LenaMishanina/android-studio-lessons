c0f2df81b68e1c111d0f7dcb0d78ab444fe27676
ElenaM <115351366+LenaMishanina@users.noreply.github.com>
2024-11-02 22:25:14 +0300

    sign in/up app -> two activities

--- MyFirstApplication/app/src/main/java/com/example/myfirstapplication/MainActivity.kt
+++ MyFirstApplication/app/src/main/java/com/example/myfirstapplication/MainActivity.kt
@@ -3,31 +3,60 @@ package com.example.myfirstapplication
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.example.myfirstapplication.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     lateinit var bind : ActivityMainBinding
+    private var login = ""
+    private var pwd = ""
+    private var firstName = ""
+    private var lastName = ""
+    private var fatherName = ""
+    private var avatarImageId = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         bind = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(bind.root)//root - контейнер, где лежат все элементы
+        setContentView(bind.root)
 
-
-        bind.btnToTestActivity1.setOnClickListener {
+        bind.btnRegister.setOnClickListener {
             val intent = Intent(this, TestActivity1::class.java)
-            intent.putExtra("key", "hello, what's your name?")// putExtra(key, value)
-            startActivityForResult(intent, 100)//100 - идентификатор запроса
+            intent.putExtra(Constance.BTN_STATE, Constance.BTN_REGISTER_STATE)
+            startActivityForResult(intent, Constance.REQUEST_CODE_REGISTER)
+        }
+
+        bind.btnLogin.setOnClickListener {
+            val intent = Intent(this, TestActivity1::class.java)
+            intent.putExtra(Constance.BTN_STATE, Constance.BTN_LOGIN_STATE)
+            startActivityForResult(intent, Constance.REQUEST_CODE_LOGIN)
         }
     }
-    // получение результата от TestActivity1
+
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
-        if (requestCode == 100 && resultCode == RESULT_OK && data != null) {
-            val request = data.getStringExtra("answer_name")
-            bind.tvMessage.text = request
-        } else if (requestCode == 100 && resultCode == RESULT_CANCELED) {
-            bind.tvMessage.text = "Вы не ввели ответ"
+        if (requestCode == Constance.REQUEST_CODE_REGISTER && resultCode == RESULT_OK && data != null) {
+            login = data.getStringExtra(Constance.KEY_LOGIN).toString()
+            pwd = data.getStringExtra(Constance.KEY_PWD).toString()
+            firstName = data.getStringExtra(Constance.KEY_FIRST_NAME).toString()
+            lastName = data.getStringExtra(Constance.KEY_LAST_NAME).toString()
+            fatherName = data.getStringExtra(Constance.KEY_FATHER_NAME).toString()
+            avatarImageId = data.getIntExtra(Constance.KEY_AVATAR, bind.imvAvatar.imageAlpha)
+            bind.tvResult.text = "Вы успешно зарегистрировались!"
+        } else if (requestCode == Constance.REQUEST_CODE_LOGIN && resultCode == RESULT_OK && data != null) {
+            if (login == data.getStringExtra(Constance.KEY_LOGIN) && pwd == data.getStringExtra(Constance.KEY_PWD)) {
+                val message = "Вы успешно вошли!\nLogin = $login;\n" +
+                        "Pwd = $pwd;\n" +
+                        "FirstName = $firstName;\n" +
+                        "LastName = $lastName;\n" +
+                        "FatherName = $fatherName;\n" +
+                        "AvatarImage = $avatarImageId."
+                bind.tvResult.text = message
+                bind.imvAvatar.visibility = View.VISIBLE
+            } else {
+                bind.tvResult.text = "Логин или пароль введены неверно"
+            }
         }
     }
+
 }
--- MyFirstApplication/app/src/main/java/com/example/myfirstapplication/TestActivity1.kt
+++ MyFirstApplication/app/src/main/java/com/example/myfirstapplication/TestActivity1.kt
@@ -1,8 +1,13 @@
 package com.example.myfirstapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Toast
 import com.example.myfirstapplication.databinding.ActivityTest1Binding
+import kotlin.math.log
 
 class TestActivity1 : AppCompatActivity() {
     lateinit var bind : ActivityTest1Binding
@@ -12,19 +17,64 @@ class TestActivity1 : AppCompatActivity() {
         bind = ActivityTest1Binding.inflate(layoutInflater)
         setContentView(bind.root)
 
-        val message = intent.getStringExtra("key")
-        bind.tvMessage.text = message
+        val signState = intent.getStringExtra(Constance.BTN_STATE)
+        Log.d("TestActivitySign", signState.toString())
 
-        bind.btnBackToMainActivity.setOnClickListener {
-            val answer = bind.edtAnswer.text.toString()
-            intent.putExtra("answer_name", answer)
-            if (answer == "") {
-                setResult(RESULT_CANCELED)
-            } else {
-                setResult(RESULT_OK, intent)
-            }
+        if (signState == Constance.BTN_LOGIN_STATE) {
+            bind.edtFirstName.visibility = View.GONE
+            bind.edtLastName.visibility = View.GONE
+            bind.edtFatherName.visibility = View.INVISIBLE
+            bind.btnSetAvatar.visibility = View.INVISIBLE
+        }
+
+        bind.btnSetAvatar.setOnClickListener {
+            Log.d("TestActivitySign", bind.imvAvatar2.visibility.toString())
+            bind.imvAvatar2.visibility = View.VISIBLE
+            Log.d("TestActivitySign", bind.imvAvatar2.visibility.toString())
+        }
+
+        bind.btnDone.setOnClickListener {
+            if (signState == Constance.BTN_REGISTER_STATE)
+                register()
+            else if (signState == Constance.BTN_LOGIN_STATE)
+                login()
+        }
+
+    }
+
+    private fun register() {
+        val login = bind.edtLogin.text.toString()
+        val pwd = bind.edtPassword.text.toString()
+        val firstName = bind.edtFirstName.text.toString()
+        val lastName = bind.edtLastName.text.toString()
+        val fatherName = bind.edtFatherName.text.toString()
+        val avatarImageId = bind.imvAvatar2.imageAlpha
+
+        if (login == "" || pwd == "" || firstName == "" || lastName == "" || fatherName == "" || bind.imvAvatar2.visibility == View.INVISIBLE) {
+            Toast.makeText(applicationContext, "Заполните все поля и выберите аватара", Toast.LENGTH_SHORT).show()
+        } else {
+            intent.putExtra(Constance.KEY_LOGIN, login)
+            intent.putExtra(Constance.KEY_PWD, pwd)
+            intent.putExtra(Constance.KEY_FIRST_NAME, firstName)
+            intent.putExtra(Constance.KEY_LAST_NAME, lastName)
+            intent.putExtra(Constance.KEY_FATHER_NAME, fatherName)
+            intent.putExtra(Constance.KEY_AVATAR, avatarImageId)
+            setResult(RESULT_OK, intent)
             finish()
         }
 
+    }
+
+    private fun login() {
+        val login = bind.edtLogin.text.toString()
+        val pwd = bind.edtPassword.text.toString()
+        if (login == "" || pwd == "") {
+            Toast.makeText(applicationContext, "Введите логин и пароль", Toast.LENGTH_SHORT).show()
+        } else {
+            intent.putExtra(Constance.KEY_LOGIN, login)
+            intent.putExtra(Constance.KEY_PWD, pwd)
+            setResult(RESULT_OK, intent)
+            finish()
+        }
     }
 }
