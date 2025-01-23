a908be1e74075214f81ce8146af70ff12312a278
ElenaM <115351366+LenaMishanina@users.noreply.github.com>
2025-01-23 19:32:34 +0300

    Array/List/ArrayList | for(each)/(do)while | Task entrepreneur

--- MyFirstApplication/app/src/main/java/com/example/myfirstapplication/MainActivity.kt
+++ MyFirstApplication/app/src/main/java/com/example/myfirstapplication/MainActivity.kt
@@ -1,36 +1,24 @@
 package com.example.myfirstapplication
 
-import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.activity.result.ActivityResult
-import androidx.activity.result.ActivityResultLauncher
-import androidx.activity.result.contract.ActivityResultContracts
-import com.example.myfirstapplication.databinding.ActivityMainBinding
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
-    lateinit var bind : ActivityMainBinding
-    lateinit var launcher: ActivityResultLauncher<Intent> // один запрос - один launcher
+    //Задача предприниматели и их заработок
+    val lostArray = arrayOf(10000, 43000, 3000, 760000)//вложил
+    val earnArray = arrayOf(7000, 35000, 50000, 120000)//заработал
+    val result = ArrayList<String>()
+    //нельзя здесь инициализировать, так как приложение еще не создано, к ресурсам нельзя обратиться
+    lateinit var names : List<String>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        bind = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(bind.root)
-
-        launcher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
-            result: ActivityResult ->
-            // request_code здесь не нужен, так как один запрос - один launcher
-            if (result.resultCode == RESULT_OK && result.data != null) {
-                val text = result.data!!.getStringExtra("key1").toString() //intent
-                bind.textView.text = text
-            }
-
+        names = resources.getStringArray(R.array.workerNames).toList()
+        for ((index, name) in names.withIndex()) {
+            result.add("$name вложила ${lostArray[index]} и заработала ${earnArray[index]}. Прибыль: ${earnArray[index] - lostArray[index]}")
         }
-
-        bind.button2.setOnClickListener {
-            val intent = Intent(this, TestActivity1::class.java)
-            launcher.launch(intent)
-        }
+        Log.d("MyLog", result.toString())
 
     }
 }
